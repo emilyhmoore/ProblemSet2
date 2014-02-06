6b615ff59037791ec0fa83225a3e87ac74beab2d
emilyhmoore <emily.moore@wustl.edu>
2014-02-06 07:31:19 -0600

    Breaking Code 4- M or D Stat 2
    Broke D stat code. Code is dependent, so it threw a warning for both
    distributions. Note that this tester() function will tell you what
    failed, which should make it easier to know how I screwed up the
    function in editing.

--- Trial.R
+++ Trial.R
@@ -259,3 +259,36 @@ tester()
 ##Throws warning for distributions indicating that something is wrong
 ##with that distribution but everything else is fine. This must mean
 ##there's an output error. Whoops! Added to the distribution at the end.
+
+
+############Breaking the code 4: D stat#################
+##Again, my code is dependent, so breaking M or D for one will break for
+##both distributions. As such, I am going to fail D this time and show
+##The warnings with that. 
+
+benfordtests<-function(x, dist=TRUE, mstat=TRUE, dstat=TRUE){
+  require(BenfordTests)##Benford Tests is supported for R 3.0.0 or later only
+  fdig<- signifd(x, 1) ##this is the first digits function. Returns first significant digit
+  n<-length(fdig) ##Calculate n 
+  digfreq<-table(c(fdig, seq(1,9)))-1 ##This is a table including the   
+  ##first signif digits in the data and a sequence of 1 to 9. 
+  ##Note: we must subtract one because otherwise adding the sequence 1:9
+  ##for the table will inflate the value for each number by 1 obs.
+  rfreq<-digfreq/n ##This is the frequency each digit is observed in the actual data
+  rfreq_hyp<-pbenf(1) ##pbenf gives the proportion we should get 
+  ##in theory if the data matches Benford. This is the same as log (1+1/d)
+  m<-sqrt(n) *max(abs(rfreq - rfreq_hyp)) ##As noted rfreq_hyp is the 
+  ##same as log (1+1/d), whereas rfreq is the relative frequency of the 
+  ##digits for the data in question. The max simply selects whichever is the max
+  ##of the digits being seen as per the formula. 
+  d<-sqrt(n)*sqrt(sum((rfreq-rfreq_hyp)^4)) ##Same as above but with sum instead for the d stat
+  reslist<-list(rfreq=rfreq, m=m, d=d)
+  everything<-all(dist, mstat, dstat)
+  if (everything==TRUE) {return(reslist)} 
+  if (everything==FALSE & dist==TRUE){print(reslist[1])}
+  if (everything==FALSE & mstat==TRUE){print(reslist[2])}
+  if(everything==FALSE & dstat==TRUE) {print(reslist[3])}
+}
+
+tester() ###Throws error for D stat for both datasets. 
+##Whoops! Accidentally raised to the 4th instead of the second!!!
